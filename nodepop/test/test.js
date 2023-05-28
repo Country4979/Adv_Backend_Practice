f85fad90a00b64773f9c3f5b3111367acbc40bfa
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-05-28 17:21:17 +0200

    Test conexión a la app Ok

--- nodepop/test/test.js
+++ nodepop/test/test.js
@@ -2,9 +2,13 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('../app');
 
-describe('Testing the root path', () => {
-  test('It should respond with a 200 status code', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-  });
+var requesting = request("http://localhost:3000")
+
+describe('products', function() {
+    describe('GET', function(){
+        it('It should respond with a 200 status code', function(done){
+            requesting.get('/')
+                .expect(200, done);
+        });
+    });
 });
